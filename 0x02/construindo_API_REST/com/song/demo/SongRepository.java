8100da693beb367477e0e38025d78fa4844359db
Thamires Cristina Santos <CIANDT\thamirescs@lnb035737spo.ciandt.global>
2022-06-27 18:35:50 -0300

    Construindo API REST

---
+++ 0x02/construindo_API_REST/com/song/demo/SongRepository.java
@@ -0,0 +1,39 @@
+package com.song.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class SongRepository {
+	
+	private List<Song> list = new ArrayList<Song>();
+	
+	public List<Song> getAllSongs(){
+		return list;
+	}
+	
+	public Song getSongById(Integer id) {
+		return list.stream().filter(song -> song.getId() == id).findFirst().orElse(null);
+	}
+	
+	public void addSong(Song s) {
+		list.add(s);
+	}
+	
+	public void updateSong(Song s) {
+		this.list.forEach(song -> {
+			if(song.getId().equals(s.getId())) {
+				song.setArtista(s.getArtista());
+				song.setAlbum(s.getAlbum());
+				song.setAnoLancamento(s.getAnoLancamento());
+			}
+		});
+	}
+	
+	public void removeSong(Song s) {
+		System.out.println("Removendo: " + list.remove(s));
+	}
+
+}
